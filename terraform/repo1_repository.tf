75c9146be2ae09656311d644e3fbef1f8474d74f
akiyama koki <koki.akiyama@tohmatsu.co.jp>
2025-01-19 08:37:55 +0000

    modify

--- terraform/repo1_repository.tf
+++
@@ -1,6 +0,0 @@
-resource "github_repository" "repo1" {
-  name               = "repo1"
-  description        = "Default repository"
-  visibility         = "private"
-  gitignore_template = "None"
-}
